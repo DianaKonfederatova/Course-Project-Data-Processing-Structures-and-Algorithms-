abf810f6e3253cde413409232c6af84cc67e9a02
Diana <konfederatovadianochka@yandex.ru>
2026-07-05 14:29:04 +0700

    feat: now uses the list writing and memory clearing functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include "database.h"
+#include "list.h"
 
 int main() {
     
@@ -12,19 +13,21 @@ int main() {
 
     std::cout << "Файл открыт и готов к чтению\n" << "\n";
     
-    struct addresses people;
+    ListNode* head = nullptr;
+    addresses people;
 
-    if (fread(&people, sizeof(struct addresses), 1, file) == 1) {
-        std::cout << "ФИО гражданина: "<<people.name << "\n";
-        std::cout << "Название улицы: "<<people.street << "\n";
-        std::cout << "Номер дома: "<<people.number_house << "\n";
-        std::cout << "Номер квартиры: "<<people.apartment << "\n";
-        std::cout << "Дата поселения: "<<people.date_of_settlement << "\n";
-   
-    } else {
-        std::cout << "Ошибка чтения файла или файл пуст\n";
+    int count = 0;
+
+    while (fread(&people, sizeof(addresses), 1, file) == 1) {
+        addList(head, people);
+        count++;
+
     }
+    
+    std::cout << "Файл успешно прочитан. В список добавлено " << count << " записей\n";
 
     fclose(file);
+    memory_clear(head);
+
     return 0;
 }
